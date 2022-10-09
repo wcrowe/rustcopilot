b585ce89277a0181a973a50c874425f3aa67502a
wcrowe <crowe.will@gmail.com>
2022-10-08 23:30:56 -0400

    add list

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+#[allow(unused_variables, dead_code)]
 use chrono::NaiveDateTime;
 
-#[allow(dead_code, unused_variables)]
+
 #[derive(Debug)]
 struct Person {
     first_name: String,
@@ -60,28 +61,61 @@ impl Person {
         now.signed_duration_since(self.dob).num_days() / 365
     }
 }
-
+#[derive(Debug)]
 struct Pepole {
     people: Vec<Person>,
 }
+impl Pepole {
+    fn new() -> Pepole {
+        Pepole { people: Vec::new() }
+    }
+    fn add(&mut self, person: Person) {
+        self.people.push(person);
+    }
+    fn remove(&mut self, index: usize) {
+        self.people.remove(index);
+    }
+    fn update(&mut self, index: usize, person: Person) {
+        self.people[index] = person;
+    }
+    fn get(&self, index: usize) -> &Person {
+        &self.people[index]
+    }
+    fn get_mut(&mut self, index: usize) -> &mut Person {
+        &mut self.people[index]
+    }
+    fn len(&self) -> usize {
+        self.people.len()
+    }
+}
+    
 
 
 fn main() {
-    let p = Person::new(
+    let p: Person = Person::new(
         "John".to_string(),
         Some("R".to_string()),
         "Doe".to_string(),
         NaiveDateTime::parse_from_str("1990-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap(),
     );
-    println!("{:?}", p);
-    println!("Full name: {}", p.full_name());
-    let mut k = p;
-    k.update(
-        Some("K".to_string()),
+    println!("{:?}", &p);
+    println!("Full name: {}", &p.full_name());
+    let mut k = Pepole::new();
+    k.add(p);
+    k.add(Person::new(
+        "Jane".to_string(),
+        None,
+        "Doe".to_string(),
+        NaiveDateTime::parse_from_str("1990-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap(),
+    ));
+    k.add(Person::new(
+        "John".to_string(),
         Some("R".to_string()),
-        Some("Doe".to_string()),
-        Some(NaiveDateTime::parse_from_str("1990-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()),
-    );
-    println!("{:?}", k);
-    println!("Full name: {}", k.full_name());
+        "Doe".to_string(),
+        NaiveDateTime::parse_from_str("1990-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap(),
+    ));
+    // print list of people
+    for (i, p) in k.people.iter().enumerate() {
+        println!("{}: {}", i, p.full_name());
+    }
 }
